4d63b0a0fcdea4d404890c09ff3b6ed896ef1582
Julia Borges <julia.borges@estat.com.br>
2023-09-29 10:56:27 -0300

    .

--- rdocs/julia_borges.R
+++ rdocs/julia_borges.R
@@ -22,3 +22,26 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+##### ORGANIZANDO O BANCO DO MATILDA #####
+
+# setwd('D:/Downloads/1994-2022')
+# library(readxl)
+# 
+# dados<- read_excel('eleicoes1994_2022.xlsx', sheet = 1)
+# dados_unicos1 <- dados[!duplicated(dados[c("ANO_ELEICAO", "SG_UF", "CD_CARGO", "SG_PARTIDO")]), ]
+# dados_unicos1 <- subset(dados_unicos1, select = -NM_MUNICIPIO)
+# 
+# 
+# dados2<- read_excel('eleicoes1994_2022.xlsx', sheet = 2)
+# dados_unicos2 <- dados2[!duplicated(dados2[c("ANO_ELEICAO", "SG_UF", "CD_CARGO", "SG_PARTIDO")]), ]
+# dados_unicos2 <- subset(dados_unicos2, select = -NM_MUNICIPIO)
+# 
+# dados_combinados <- rbind(dados_unicos1, dados_unicos2)
+# 
+# library(openxlsx)
+# 
+# # Nome do arquivo de saída
+# arquivo_saida <- "resultados.xlsx"
+# 
+# write.xlsx(dados_combinados, file = arquivo_saida, sheetName = "1994-2022")
